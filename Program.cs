bda57a58170662a24dcbd077d4c6b50e5a0541fe
Shawn-Engineer <101727939+ShawnEngineer2@users.noreply.github.com>
2024-11-12 08:20:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JumpGame jumpGame = new JumpGame();
+            jumpGame.PlayGame();
+            
+            RestAPI restAPI = new RestAPI();
+            restAPI.GetRestData();
+        }
+    }
+}
